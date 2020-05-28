ff8e270e0e8ed55abb79284fd99e499c5c65c749
Ivan Riskov <riskovivan94@gmail.com>
2020-05-28 17:20:12 +0500

    Experiments with pug files
    Added pug code to several separate files, made minimal modular structure;
    Also changes some styles in sass;
    Minor changes in index.js (edited selectors of elements)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import * as $ from 'jquery'
 import WebpackLogo from '@/assets/icon.png'
 import './styles/styles.sass'
 
-const title1 = document.querySelector('h1')
+const title1 = document.querySelector('.item-title')
 
 const image = document.createElement('img');
 image.src = WebpackLogo;
 image.className = 'webpack-image';
 title1.after(image);
 
-$("h1").click(function () {
+$(".item-descr").click(function () {
   $(this).text('Clicked!');
 });
