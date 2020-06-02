ad6d499c5ab7cc356bf328412b0de7a4ed2a2982
Ivan Riskov <riskovivan94@gmail.com>
2020-06-02 20:00:07 +0500

    add scss variables, colors & type is PerfectPixel now
    also changed experimental script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import * as $ from 'jquery'
 import './styles/styles.scss'
 
-$("h2").click(function () {
-  $(this).css('color', '#aa0000');
+$('h2').click(function () {
+  switch ($(this).css('color')) {
+    case 'rgb(31, 32, 65)':
+      $(this).css('color', 'rgb(188, 156, 255)')
+      break
+    case 'rgb(188, 156, 255)':
+      $(this).css('color', 'rgb(111, 207, 151)')
+      break
+    case 'rgb(111, 207, 151)':
+      $(this).css('color', 'rgb(31, 32, 65)')
+      break
+  }
 });
