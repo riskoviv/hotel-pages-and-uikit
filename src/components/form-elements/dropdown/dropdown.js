5950d117dca062a7acff266730fd53b7cda61a1f
Ivan Riskov <riskovivan94@gmail.com>
2020-07-14 22:20:30 +0500

    Added some options to iqd script, added styles to dropdown

--- src/components/form-elements/dropdown/dropdown.js
+++ src/components/form-elements/dropdown/dropdown.js
@@ -7,5 +7,26 @@ import '../../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdo
 import '../../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
 $(document).ready(() => {
-  $('.iqdropdown').iqDropdown();
+  $('.iqdropdown').iqDropdown({
+    setSelectionText(itemCount, totalItems) {
+      if (!totalItems) {
+        return 'Сколько гостей';
+      }
+      const usePlural = totalItems !== 1 && this.textPlural.length > 0;
+      const text = usePlural ? this.textPlural : this.selectionText;
+      return `${totalItems} ${text}`;
+    },
+    onChange: (id, count, totalItems) => {
+      // console.log(maxCount);
+      $('.button-increment').addClass('button-increment_disabled');
+      // if (count === maxCount) {
+      //   $(`${id} .button-increment`).addClass('button-increment_disabled')
+      // }
+      // else if (count === minCount) {
+      //   $(`${id} .button-decrement`).addClass('button-decrement_disabled')
+      // }
+    },
+  });
+  $('.icon-decrement').text('-');
+  $('.icon-increment').text('+');
 });
