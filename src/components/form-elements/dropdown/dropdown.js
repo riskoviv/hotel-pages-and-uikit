e70aaad9d27a94a56840fd3973cb243bd0349182
Ivan Riskov <riskovivan94@gmail.com>
2020-07-15 13:22:23 +0500

    Changed logic of changing of dropdown's caption; buton disabling
    Added separate counting of infants in dropdown's caption;
    Added more accurate setting of caption depending on count of guests and infants;
    Changed button disabling logic and styles: made "disabled" attribute adding to button instead of adding a class "button-(in|de)crement_disabled".

--- src/components/form-elements/dropdown/dropdown.js
+++ src/components/form-elements/dropdown/dropdown.js
@@ -3,38 +3,61 @@ import '../../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdo
 
 $(document).ready(() => {
   $('.iqdropdown').iqDropdown({
-    setSelectionText(itemCount, totalItems) {
-      if (!totalItems) {
+    setSelectionText(guestsCount, totalGuests) {
+      if (!totalGuests) {
         return 'Сколько гостей';
       }
-      const numEnd = parseInt(totalItems.toString().split('').pop())
-      
-      const usePlural = totalItems !== 1 && this.textPlural.length > 0;
-      let text = usePlural ? this.textPlural : this.selectionText;
-      if ([2, 3, 4].indexOf(numEnd) != -1 && totalItems < 12) {
-        text = 'гостя'
-      } else if (totalItems === 1) {
-        text = 'гость'
-      }
 
-      return `${totalItems} ${text}`;
+      const infantsCount = guestsCount.item3
+
+      const checkGuests = (totalItems) => {
+        const totalItemsEnd = parseInt(totalItems.toString().split('').pop())
+        const infantsCountEnd = parseInt(infantsCount.toString().split('').pop())
+        const usePlural = totalItems !== 1 && this.textPlural.length > 0;
+
+        let text = usePlural ? this.textPlural : this.selectionText;
+        if ([2, 3, 4].indexOf(totalItemsEnd) != -1 && (totalItems < 12 || totalItems > 21)) {
+          text = 'гостя'
+        } else if (totalItems === 1 || totalItemsEnd === 1 && totalItems > 20) {
+          text = 'гость'
+        }
+        
+        let textInfants = ''
+        if ([2, 3, 4].indexOf(infantsCountEnd) != -1 && (infantsCount < 12 || infantsCount > 21)) {
+          textInfants = 'младенца'
+        } else if (infantsCount === 1 || infantsCountEnd === 1 && infantsCount > 20) {
+          textInfants = 'младенец'
+        } else {
+          textInfants = 'младенцев'
+        }
+
+        return [text, textInfants]
+      }
+      
+      if (infantsCount > 0) {
+        const text = checkGuests(totalGuests - infantsCount)[0]
+        const textInfants = checkGuests(totalGuests - infantsCount)[1]
+        return `${totalGuests - infantsCount} ${text}, ${infantsCount} ${textInfants}`
+      } else {
+        return `${totalGuests} ${checkGuests(totalGuests)[0]}`;
+      }
     },
     onChange: (id, count, totalItems) => {
       const buttonIncrement = $(`[data-id='${id}'] .button-increment`)
       const buttonDecrement = $(`[data-id='${id}'] .button-decrement`)
       if (count === $(`[data-id='${id}']`).data('maxcount')) {
-        buttonIncrement.addClass('button-increment_disabled')
+        buttonIncrement.prop('disabled', true)
       }
       else if (!count) {
-        buttonDecrement.addClass('button-decrement_disabled')
+        buttonDecrement.prop('disabled', true)
       }
       else {
-        buttonIncrement.removeClass('button-increment_disabled')
-        buttonDecrement.removeClass('button-decrement_disabled')
+        buttonIncrement.removeAttr('disabled')
+        buttonDecrement.removeAttr('disabled')
       }
     },
   });
-  $('.button-decrement').addClass('button-decrement_disabled');
+  $('.button-decrement').prop('disabled', true);
   $('.icon-decrement').text('-');
   $('.icon-increment').text('+');
 });
