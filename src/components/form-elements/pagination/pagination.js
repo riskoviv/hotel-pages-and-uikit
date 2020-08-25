4521355e36301e6ce63ced0890919b86bf46863c
Ivan Riskov <riskovivan94@gmail.com>
2020-08-25 12:52:31 +0500

    pagination: finished reprogramming of plugin
    added function that removes excess page numbers to display only 2 adjacent page numbers near the active page number;
    modified function that displays caption text about what numbers of elements are displayed on the current page: made right displaying of numbers of elements on the last page

--- src/components/form-elements/pagination/pagination.js
+++ src/components/form-elements/pagination/pagination.js
@@ -3,12 +3,12 @@ const pagination = require('paginationjs')
 
 $(() => {
   function simpleTemplating(data) {
-      var html = '<ul class="pagination__items-list">';
+      var html = '<ul class="pagination__items-list">'
       $.each(data, function(index, item){
-          html += '<li class="pagination__item">'+ item +'</li>';
-      });
-      html += '</ul>';
-      return html;
+          html += '<li class="pagination__item">'+ item +'</li>'
+      })
+      html += '</ul>'
+      return html
   }
 
   function addClassesToElements() {
@@ -16,20 +16,42 @@ $(() => {
     $('.paginationjs-next > a').addClass('material-icons')
   }
 
+  let $paginationNumberLastNum
+  
+  function removeExcessPageNumbers() {
+    const $pageNumbers = $('.paginationjs-page:not(.paginationjs-last):not(.paginationjs-first)')
+    const $activePage = $('.paginationjs-page.active')
+    const $ellipsis = $('.paginationjs-ellipsis')
+    for (let pageNumber of $pageNumbers) {
+      let activePageNum = $activePage.data('num')
+      let pageNumberNum = $(pageNumber).data('num')
+      if ((pageNumberNum > activePageNum + 2
+        || pageNumberNum < activePageNum - 2)
+        && pageNumberNum !== 1
+        && pageNumberNum !== $paginationNumberLastNum
+      )
+        $(pageNumber).css('display', 'none')
+    }
+    if ($activePage.data('num') === 5)
+      $ellipsis.clone().insertAfter('.paginationjs-page[data-num="1"]')
+    if ($activePage.data('num') === $paginationNumberLastNum-4)
+      $ellipsis.clone().insertBefore($('.paginationjs-page:last'))
+  }
+
   const $paginator = $('.pagination__container')
   
   $paginator.pagination({
     dataSource: function(done){
-      let result = [];
-      for (let i = 1; i <= 180; i++) {
-        result.push(i);
+      let result = []
+      for (let i = 1; i <= 175; i++) {
+        result.push(i)
       }
-      done(result);
+      done(result)
     },
     callback: function(data, pagination) {
       // template method of yourself
-      var html = simpleTemplating(data);
-      $('.pagination__data-container').html(html);
+      var html = simpleTemplating(data)
+      $('.pagination__data-container').html(html)
     },
     pageSize: 12,
     autoHidePrevious: true,
@@ -40,14 +62,18 @@ $(() => {
     pageRange: 2,
     afterRender: function () {
       addClassesToElements()
+      removeExcessPageNumbers()
       const navData = $('.paginationjs-nav').text().split(',')
-      $('.paginationjs-nav')
-        .text(`${navData[0] * 12 - 11} - ${navData[0] * 12} из ${navData[1]} вариантов аренды`)
+      if (navData[0] == $paginationNumberLastNum)
+        $('.paginationjs-nav')
+          .text(`${navData[0] * 12 - 11} - ${navData[1]} из ${navData[1]} вариантов аренды`)
+      else
+        $('.paginationjs-nav')
+          .text(`${navData[0] * 12 - 11} - ${navData[0] * 12} из ${navData[1]} вариантов аренды`)
     },
     formatNavigator: '<%= currentPage %>,<%= totalNumber %>',
-    showNavigator: true,
+    showNavigator: true
   })
 
-  
-
+  $paginationNumberLastNum = $('.paginationjs-last').data('num')
 })
