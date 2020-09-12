e22509c614543d9da9a072749e51f0435d3bd55d
Ivan Riskov <riskovivan94@gmail.com>
2020-09-13 00:06:23 +0500

    Updates

---
+++ colors-and-type~common~form-elements.50d328c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1:function(n,u,e){"use strict";e.r(u),e.d(u,"requireAll",(function(){return i}));e(5),e(6);function i(n){return n.keys().map(n)}},6:function(n,u,e){}}]);
