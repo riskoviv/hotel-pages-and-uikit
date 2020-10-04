461ffa1fa37891716f7ab7210ac2b26228352552
Ivan Riskov <riskovivan94@gmail.com>
2020-10-04 23:02:08 +0500

    form-elements: refactoring in progress
    change markup & styles of components and placing them on page

--- src/components/form-elements/like-button/like-button.js
+++ src/components/form-elements/like-button/like-button.js
@@ -1,17 +1,17 @@
 const checkLike = (likeButton) => {
-  const likeButtonHeart = likeButton.querySelector('.button_like__heart');
-  const likeButtonCount = likeButton.querySelector('.button_like__count');
+  const likeButtonHeart = likeButton.querySelector('.like-button__heart');
+  const likeButtonCount = likeButton.querySelector('.like-button__count');
   let likesCount = parseInt(likeButtonCount.textContent);
-  if (likeButton.classList.contains('button_like_clicked')) {
+  if (likeButton.classList.contains('like-button_clicked')) {
     likeButtonHeart.textContent = 'favorite';
     likeButtonCount.textContent = (++likesCount).toString();
   }
 }
 
 const setLike = (likeButton) => {
-  likeButton.classList.toggle('button_like_clicked');
-  const likeButtonHeart = likeButton.querySelector('.button_like__heart');
-  const likeButtonCount = likeButton.querySelector('.button_like__count');
+  likeButton.classList.toggle('like-button_clicked');
+  const likeButtonHeart = likeButton.querySelector('.like-button__heart');
+  const likeButtonCount = likeButton.querySelector('.like-button__count');
   let likesCount = parseInt(likeButtonCount.textContent);
   if (likeButtonHeart.textContent === 'favorite') {
     likeButtonHeart.textContent = 'favorite_border';
@@ -22,7 +22,7 @@ const setLike = (likeButton) => {
   }
 }
 
-const likeButtons = document.querySelectorAll('.button_like');
+const likeButtons = document.querySelectorAll('.like-button');
 
 
 for (likeButton of likeButtons) {
