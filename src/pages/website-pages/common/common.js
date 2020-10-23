5efc1465ee85e234c3ac1874a8d7fd66812b611d
Ivan Riskov <riskovivan94@gmail.com>
2020-10-23 17:35:22 +0500

    landing & search-room: move common code to common.js
    that was created in website-pages/common

---
+++ src/pages/website-pages/common/common.js
@@ -0,0 +1,8 @@
+import { requireAll } from '../../ui-kit/common/common';
+
+// pattern to take each .js & .scss files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
+requireAll(require.context('../../../layouts/website-page', false, /^\.\/(?!.*(?:__tests__)).*\.(scss)$/));
+requireAll(require.context('../../../components/common', true, /^\.\/(?!.*(?:__tests__)).*\.(scss)$/));
+requireAll(require.context('../../../components/form-elements', true, /^\.\/(?!.*(?:__tests__)).*\.(js)|(scss)$/));
+requireAll(require.context('../../../components/cards', true, /^\.\/(?!.*(?:__tests__)).*\.(js)|(scss)$/));
+requireAll(require.context('../../../components/headers-and-footers', true, /^\.\/(?!.*(?:__tests__)).*\.(js)|(scss)$/));
