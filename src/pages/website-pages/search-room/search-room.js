fd646dc21fbc6cb0b3c189241515fc8d7aa7266e
Ivan Riskov <riskovivan94@gmail.com>
2020-10-31 10:58:13 +0500

    landing, room-details, search-room: change main imports
    change imports to pages chunks from ui-kit's common to website-pages' common

--- src/pages/website-pages/search-room/search-room.js
+++ src/pages/website-pages/search-room/search-room.js
@@ -1,4 +1,4 @@
-import { requireAll } from '../../ui-kit/common/common';
+import { requireAll } from '../common/common';
 
 // pattern to take each .js & .scss files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
 requireAll(require.context('./', true, /^\.\/(?!.*(?:__tests__)).*\.(scss)$/));
