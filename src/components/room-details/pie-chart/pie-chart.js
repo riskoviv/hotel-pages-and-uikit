09e34acaf58e14144376cd7c60bdd97167646833
Ivan Riskov <riskovivan94@gmail.com>
2020-11-02 22:49:33 +0500

    change class name according to html; change whitespace value check

--- src/components/room-details/pie-chart/pie-chart.js
+++ src/components/room-details/pie-chart/pie-chart.js
@@ -2,9 +2,9 @@ require('@vendor/peity/jquery.peity.min');
 
 $(() => {
   const colors = ["purple", "green", "orange", "black"];
-  $('.pie-chart').peity('donut', {
+  $('.pie-chart__pie').peity('donut', {
     fill: function (value) {
-      return value < 1 ? "white" : colors.shift();
+      return value === 1 ? "white" : colors.shift();
     }
   });
 });
