98b8613d6b82e8fcd55207a42ad8be61c4018364
Ivan Riskov <riskovivan94@gmail.com>
2020-11-13 23:23:28 +0500

    rename login to sign-in everywhere; wrap header buttons in links

---
+++ src/components/cards/sign-in-card/sign-in-card.js
@@ -0,0 +1,16 @@
+$(() => {
+  const $registrationButton = $('.js-sign-in-card__registration-button');
+  const $registrationCard = $('.registration-and-sign-in__registration-card');
+  const $signInCard = $('.registration-and-sign-in__sign-in-card');
+  const registrationHiddenClass = 'registration-and-sign-in__registration-card_hidden';
+  const signInHiddenClass = 'registration-and-sign-in__sign-in-card_hidden';
+
+  function showRegistrationCard() {
+    $signInCard.addClass(signInHiddenClass);
+    $registrationCard.removeClass(registrationHiddenClass);
+  }
+  
+  $registrationButton.on('click', function () {
+    showRegistrationCard();
+  });
+});
