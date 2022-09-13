5c4af546d0f9fb6f446a6e0f7bbc9f9312c9c963
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-09-13 22:17:03 +0500

    pie-chart: rewrite init logic, remove kludges
    • make fill gradients array based on values and use in-plugin option 'fill' to assign gradients to svg paths instead of adding ids to paths to assign gradients or white color to white spaces;
    • remove transform: scaleX(-1) to flip chart horizontally because pie-chart in design markup rendered counterclockwise, but peity renders it clockwise. Instead reverse array of values in pie-chart mixin and just set values in reverse order (to use mixin, values are still passed in it in normal order);
    • remove data-whitespace attribute because it's not needed now to search for white-spaces in chart;
    • add class w/ js- prefix to .pie-chart__pie to get it in js

--- src/components/room-details/pie-chart/pie-chart.js
+++ src/components/room-details/pie-chart/pie-chart.js
@@ -1,60 +1,35 @@
+// eslint-disable-next-line no-unused-vars
 import peity from 'peity';
 
 $(() => {
-  let colors = ['orange', 'green', 'purple', 'black'];
-
-  const $pieInitElement = $('.pie-chart__pie');
-
-  if ($pieInitElement.length !== 0) {
-    const whiteSpace = $pieInitElement.data('whitespace');
-
-    $.fn.peity.defaults.donut = {
-      fill: ['#bc9cff', '#bc9cff80'],
-    };
-
-    $pieInitElement.peity('donut');
-
-    const $peitySvg = $('.peity');
-    const values = $($pieInitElement).data('values');
-    const zeroIndexes = [];
-
-    values.forEach(function (item, index) {
-      if (item === 0) {
-        zeroIndexes.push(index);
-      }
-    });
-
-    if (zeroIndexes.length !== 0) {
-      colors = colors.filter((colorItem, colorIndex, colorsArray) => {
-        return (zeroIndexes.indexOf(colorIndex) === -1);
-      });
-    }
-
-    if (colors.length > 0) {
-      (function addIDs() {
-        const $paths = $peitySvg.find(`path:not([data-value="${whiteSpace}"])`);
-        $paths.each(function (index) {
-          $(this).attr('id', colors[index]);
-        });
-        const $pathsWhitespaces = $peitySvg.find(`path[data-value="${whiteSpace}"]`);
-        $pathsWhitespaces.addClass('peity__whitespace');
-      }());
-
-      const gradients = `
-        ${colors.map((color) => {
-          return `
-            <linearGradient id="${color}-gradient" x1="0" x2="0" y1="0" y2="1">
-              <stop class=${color}-stop1 offset="0%"></stop>
-              <stop class=${color}-stop2 offset="100%"></stop>
-            </linearGradient>
-          `;
-        }).join('')}
-      `;
-
-      const ns = 'http://www.w3.org/2000/svg';
-      const $defs = $(document.createElementNS(ns, 'defs'));
-      $defs.html(gradients);
-      $peitySvg.prepend($defs);
-    }
+  const $pieInitElement = $('.js-pie-chart__pie');
+  if ($pieInitElement.length === 0) {
+    return;
   }
+
+  const colors = ['black', 'purple', 'green', 'orange'];
+  const values = $pieInitElement.data('values');
+  const fillGradients = values.flatMap((value, index) => {
+    if (value > 0) {
+      return [`url(#${colors[index]}-gradient) ${colors[index]}`, '#fff'];
+    }
+    return null;
+  }).filter((fill) => fill !== null);
+
+  $pieInitElement.peity('donut', {
+    fill: fillGradients,
+  });
+
+  const gradients = colors.map((color) => `
+    <linearGradient id="${color}-gradient" x1="0" x2="0" y1="0" y2="1">
+      <stop class="${color}-stop1" offset="0%"></stop>
+      <stop class="${color}-stop2" offset="100%"></stop>
+    </linearGradient>
+  `).join('');
+
+  const $peitySvg = $('.peity');
+  const ns = 'http://www.w3.org/2000/svg';
+  const $defs = $(document.createElementNS(ns, 'defs'));
+  $defs.html(gradients);
+  $peitySvg.prepend($defs);
 });
