9ded22558ff0c81e98bbb0e0dcd0a5630455b2ad
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-09-14 14:03:40 +0500

    rate-button.js: improve setRating()
    • now it's possible to set any rating to rate-button from 0 to 5 (previously lesser rating could not be set
    • also expose the function adding it to window
    • add jsdoc comment w/ fn & params description & example

--- src/components/form-elements/rate-button/rate-button.js
+++ src/components/form-elements/rate-button/rate-button.js
@@ -1,16 +1,19 @@
+/**
+ * Function to dynamically set rating for rate-button element.
+ * To use it, call somewhere in external script w/ id of rate-button element and raring value.
+ * @example
+ * window.setRating('rating-746', 3);
+ * setRating('rating-042', 5);
+ * @param {string} rateButtonID ID of rate button element
+ * @param {number} rating number from 0 to 5
+ */
 function setRating(rateButtonID, rating) {
   const rateButton = document.querySelector(`.rate-button#${rateButtonID}`);
   const stars = rateButton.querySelectorAll('.rate-button__star');
-  for (let i = 0; i < rating; i++) {
-    stars[i].textContent = 'star';
-  }
+  stars.forEach((starElem, index) => {
+    // eslint-disable-next-line no-param-reassign
+    starElem.textContent = (index < rating) ? 'star' : 'star_border';
+  });
 }
 
-export { setRating };
-
-/*
-    For setting rate-button's ratings (count of filled stars) from external script:
-*/
-// import { setRating } from '../../components/form-elements/rate-button/rate-button'
-// setRating('opinion_1', 3)
-// setRating('opinion_2', 5)
+window.setRating = setRating;
