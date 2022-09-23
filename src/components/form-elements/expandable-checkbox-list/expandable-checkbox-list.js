23993f338aa867383a9b37e7f6f0bd6772308b21
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-09-23 23:13:26 +0500

    expandable-checkbox-list: implementation w/o js
    re-implement using <details> & <summary> elements, remove js implementation using class toggle + display none/block

--- src/components/form-elements/expandable-checkbox-list/expandable-checkbox-list.js
+++
@@ -1,8 +0,0 @@
-const expandableCheckboxListsToggles = document.querySelectorAll('.js-expandable-checkbox-list__toggle');
-const expandableCheckboxListsToggleHandler = (e) => {
-  e.currentTarget.parentNode.classList.toggle('expandable-checkbox-list_opened');
-};
-
-expandableCheckboxListsToggles.forEach((toggle) => {
-  toggle.addEventListener('click', expandableCheckboxListsToggleHandler);
-});
