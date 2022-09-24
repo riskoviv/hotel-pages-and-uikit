04c730b46c73920391f872ddbe8ab04616b55dfc
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-09-24 23:38:10 +0500

    like-button: change implementation
    • Instead of changing values by js, use css counter & change content of pseudo-elements:
    if root element has modifier class _clicked, color of heart & number element sets to purple,
    value of :before element is set by counter and increased automatically if root element has modifier class _clicked.
    • JS now used here only to add event listeners to buttons.
    • remove spans __heart & __count, use pseudo-elements instead;
    • rename __body to __content;
    • add style attr w/ --likes-count prop that contains initial likes count value,
    • changing --likes-count prop afterwards changes likes count displayed inside the button considering whether button clicked or not (if clicked, displayed value will be +1, thanks to css counter)

--- src/components/form-elements/like-button/like-button.js
+++ src/components/form-elements/like-button/like-button.js
@@ -1,33 +1,9 @@
-const checkLike = (likeButton) => {
-  const likeButtonHeart = likeButton.querySelector('.like-button__heart');
-  const likeButtonCount = likeButton.querySelector('.like-button__count');
-  let likesCount = parseInt(likeButtonCount.textContent, 10);
-  if (likeButton.classList.contains('like-button_clicked')) {
-    likeButtonHeart.textContent = 'favorite';
-    likeButtonCount.textContent = String(likesCount += 1);
-  }
-};
-
-const setLike = (likeButton) => {
-  likeButton.classList.toggle('like-button_clicked');
-  const likeButtonHeart = likeButton.querySelector('.like-button__heart');
-  const likeButtonCount = likeButton.querySelector('.like-button__count');
-  let likesCount = parseInt(likeButtonCount.textContent, 10);
-  if (likeButtonHeart.textContent === 'favorite') {
-    likeButtonHeart.textContent = 'favorite_border';
-    likeButtonCount.textContent = String(likesCount -= 1);
-  } else {
-    likeButtonHeart.textContent = 'favorite';
-    likeButtonCount.textContent = String(likesCount += 1);
-  }
-};
-
 const likeButtons = document.querySelectorAll('.like-button');
 
+const handleLikeButtonClick = (e) => {
+  e.currentTarget.classList.toggle('like-button_clicked');
+};
+
 [...likeButtons].forEach((likeButton) => {
-  // запустить проверку на лайк
-  checkLike(likeButton);
-  likeButton.addEventListener('click', (e) => {
-    setLike(e.currentTarget);
-  });
+  likeButton.addEventListener('click', handleLikeButtonClick);
 });
