6619f562841954874fb79b568b589afbb667c1d5
Ivan Riskov <44531490+riskoviv@users.noreply.github.com>
2022-09-27 23:07:44 +0500

    registration-card: use js-classes

--- src/components/cards/registration-card/registration-card.js
+++ src/components/cards/registration-card/registration-card.js
@@ -1,7 +1,7 @@
 $(() => {
   const $signInButton = $('.js-registration-card__sign-in-button');
-  const $registrationCard = $('.registration-and-sign-in__registration-card');
-  const $signInCard = $('.registration-and-sign-in__sign-in-card');
+  const $registrationCard = $('.js-registration-and-sign-in__registration-card');
+  const $signInCard = $('.js-registration-and-sign-in__sign-in-card');
   const registrationHiddenClass = 'registration-and-sign-in__registration-card_hidden';
   const signInHiddenClass = 'registration-and-sign-in__sign-in-card_hidden';
 
